01059ffd6e602ec9695ec655ebb4be9671a7702f
Joseph Salazar <86029240+namejoseph@users.noreply.github.com>
2023-03-15 01:35:29 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+
+const child = document.getElementById("rotators");
+
+const lesserValue = Math.min(window.innerWidth * 0.85, window.innerHeight * 0.85);
+child.style.width = lesserValue + 'px';
+child.style.height = lesserValue + 'px';
